414f0cec7ae3db4e607b203613f9d052acdb960a
dev_nick <developernikhil28@gmail.com>
2026-02-04 20:04:12 +0530

    Pages and routes setup

---
+++ next-auth-app/src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+
+
+
+export default function SignUpPage(){
+    return (
+        <div>SignUp Page</div>
+    )
+}
