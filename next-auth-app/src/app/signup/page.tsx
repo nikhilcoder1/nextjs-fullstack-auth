f10d041c9cd1f27d7ba852a65328531705dbc76a
dev_nick <developernikhil28@gmail.com>
2026-02-05 02:31:37 +0530

    onSignUp functionality achieved

--- next-auth-app/src/app/signup/page.tsx
+++ next-auth-app/src/app/signup/page.tsx
@@ -1,20 +1,50 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function SignUpPage() {
+
+  const router = useRouter();
+
   const [user, setUser] = useState({
     username: "",
     email: "",
     password: "",
   });
 
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) 
+      {
+        setButtonDisabled(false);
+      }else 
+      {
+        setButtonDisabled(true);
+      }
+  }, [user])
+  
+
   const onSignUp = async () => {
-    try {
-    } catch (error) {}
-  };
+    try 
+    {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      router.push("/login");            
+
+    } catch (error:any) {
+      console.log("Signup failed", error.message);
+      toast.error(error.message);
+
+    } finally {
+      setLoading(false);
+    }
+};
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 px-4">
@@ -23,9 +53,8 @@ export default function SignUpPage() {
         <h1 className="text-3xl font-bold text-gray-100 text-center">
           Create Account
         </h1>
-        <p className="text-sm text-gray-400 text-center mt-2">
-          Sign up to get started
-        </p>
+
+        <h1 className="text-sm text-gray-400 text-center mt-2">{loading ? "Processing" : "Signup to get started"}</h1>
 
         {/* Form */}
         <div className="mt-8 space-y-5">
@@ -93,8 +122,9 @@ export default function SignUpPage() {
           <button
             onClick={onSignUp}
             className="w-full py-2.5 rounded-lg bg-blue-600 text-white font-medium hover:bg-blue-700 active:scale-[0.98] transition focus:outline-none focus:ring-2 focus:ring-blue-500"
+
           >
-            Sign Up
+            {buttonDisabled ? "Please fill all fields" : "Sign Up"}
           </button>
         </div>
 
