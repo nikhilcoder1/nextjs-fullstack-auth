4cbfc8e69449d22b0ab01e126f34baae7911f9de
dev_nick <developernikhil28@gmail.com>
2026-02-05 21:26:17 +0530

    GetUserDetails Button and its functionality added

--- next-auth-app/src/app/profile/page.tsx
+++ next-auth-app/src/app/profile/page.tsx
@@ -1,10 +1,56 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function ProfilePage() {
+
+  const router = useRouter();
+  const [data, setData] = useState("nothing")
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+      toast.error(
+        error instanceof Error
+          ? error.message
+          : "An error occurred during logout"
+      );
+    }
+  };
+
+  const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setData(res.data.data._id)
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 px-4">
       <div className="bg-gray-800 border border-gray-700 rounded-2xl shadow-lg px-10 py-8">
         <h1 className="text-2xl font-semibold text-gray-100 text-center">
           Profile Page
         </h1>
+        <hr />
+        <h2 className="p-1 rounded bg-green-500">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}
+        </Link>}</h2>
+        <button
+          onClick={logout}
+          className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Logout
+        </button>
+
+        <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >GetUser Details</button>
       </div>
     </div>
   );
