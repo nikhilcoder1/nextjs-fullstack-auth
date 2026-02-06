a6261335b3331a88c4c46c14fc6115050a9ed855
dev_nick <developernikhil28@gmail.com>
2026-02-06 23:45:24 +0530

    fix(auth): adjust signup route for verification flow

--- next-auth-app/src/app/api/users/signup/route.ts
+++ next-auth-app/src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connectToDatabse } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 connectToDatabse();
 
@@ -28,6 +29,9 @@ export async function POST(request: NextRequest){
         })
 
         const savedUser = await newUser.save()
+        
+        // send verification email
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
 
         return NextResponse.json({
             message: "User created successfully",
